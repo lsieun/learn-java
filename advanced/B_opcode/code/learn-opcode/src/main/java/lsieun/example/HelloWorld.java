55d6914d91b9d89960f977c1d500bde422ed7466
liusen <331505785@qq.com>
2019-03-11 22:37:01 +0800

    20190311

--- advanced/B_opcode/code/learn-opcode/src/main/java/lsieun/example/HelloWorld.java
+++ advanced/B_opcode/code/learn-opcode/src/main/java/lsieun/example/HelloWorld.java
@@ -1,9 +1,16 @@
 package lsieun.example;
 
+import java.util.Arrays;
+
 public class HelloWorld {
     private static final int i = 10;
 
     public void test() {
         System.out.println("test!");
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[65536];
+        System.out.println(array.length);
     }
 }
